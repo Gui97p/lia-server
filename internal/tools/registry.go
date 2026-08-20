dcffc0a70d50ec5e2452d1dae411ad8e9c62d907
Gui97p <guipassaroto07@gmail.com>
2026-08-20 11:26:55 -0300

    tools registry base

--- internal/tools/registry.go
+++ internal/tools/registry.go
@@ -1,1 +1,26 @@
 package tools
+
+import (
+	"context"
+
+	"github.com/Gui97p/lia-server/internal/session"
+)
+
+type Handler func(ctx context.Context, sess *session.Session, params map[string]any) (session.ToolResult, error)
+
+type Registry struct {
+	handlers map[string]Handler
+}
+
+func NewRegistry() *Registry {
+	return &Registry{handlers: make(map[string]Handler)}
+}
+
+func (r *Registry) Register(name string, h Handler) {
+	r.handlers[name] = h
+}
+
+func (r *Registry) Get(name string) (Handler, bool) {
+	h, ok := r.handlers[name]
+	return h, ok
+}
