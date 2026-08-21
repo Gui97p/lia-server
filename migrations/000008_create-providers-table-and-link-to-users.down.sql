5bf65890ef38fe9c06c2a282c2cabe251e5690d3
Gui97p <guipassaroto07@gmail.com>
2026-08-21 11:47:37 -0300

    changed migrations to refer each provider as a row

--- migrations/000008_create-providers-table-and-link-to-users.down.sql
+++ migrations/000008_create-providers-table-and-link-to-users.down.sql
@@ -2,9 +2,9 @@ ALTER TABLE users
 ADD COLUMN groq_api_key_encrypted TEXT;
 
 UPDATE users
-SET groq_api_key_encrypted = providers.groq_api_key_encrypted
+SET groq_api_key_encrypted = providers.encrypted_key
 FROM providers
-WHERE users.id = providers.user_id AND providers.groq_api_key_encrypted IS NOT NULL;
+WHERE users.id = providers.user_id AND providers.encrypted_key IS NOT NULL AND providers.provider = 'groq';
 
 DROP TRIGGER providers_updated_at ON providers;
 DROP TABLE providers;
--- migrations/000008_create-providers-table-and-link-to-users.up.sql
+++ migrations/000008_create-providers-table-and-link-to-users.up.sql
@@ -1,19 +1,21 @@
 CREATE TABLE providers (
-    user_id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
 
-    groq_api_key_encrypted TEXT,
-    gemini_api_key_encrypted TEXT,
+    provider TEXT NOT NULL,
+    encrypted_key TEXT NOT NULL,
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (user_id, provider)
 );
 
 CREATE TRIGGER providers_updated_at
 BEFORE UPDATE ON providers
 FOR EACH ROW EXECUTE FUNCTION set_updated_at();
 
-INSERT INTO providers (user_id, groq_api_key_encrypted)
-SELECT id, groq_api_key_encrypted FROM users
+INSERT INTO providers (user_id, provider, encrypted_key)
+SELECT id, 'groq', groq_api_key_encrypted FROM users
 WHERE groq_api_key_encrypted IS NOT NULL;
 
 ALTER TABLE users
