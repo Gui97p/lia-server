47859ba512cc6007ea59d0ab08b9347a2540a58c
Gui97p <guipassaroto07@gmail.com>
2026-09-14 14:20:11 -0300

    emojis & extra space filtering from TTS

--- internal/audio/tts.go
+++ internal/audio/tts.go
@@ -3,9 +3,19 @@ package audio
 import (
 	"bytes"
 	"context"
+	"regexp"
+	"strings"
 
 	edgetts "github.com/foresturquhart/edge-tts"
 )
+
+var emojiPattern = regexp.MustCompile(`[\x{1F300}-\x{1FAFF}\x{2600}-\x{27BF}\x{2190}-\x{21FF}\x{2B00}-\x{2BFF}\x{FE0F}]`)
+var extraSpacePattern = regexp.MustCompile(`[ \t]{2,}`)
+
+func stripEmoji(text string) string {
+	stripped := emojiPattern.ReplaceAllString(text, "")
+	return strings.TrimSpace(extraSpacePattern.ReplaceAllString(stripped, " "))
+}
 
 type TTSClient interface {
 	Synthesize(ctx context.Context, text string) ([]byte, error)
@@ -23,7 +33,7 @@ func (c *EdgeTTSClient) Synthesize(ctx context.Context, text string) ([]byte, er
 	config := edgetts.DefaultConfig()
 	config.Voice = c.Voice
 
-	comm, err := edgetts.NewCommunicate(text, config)
+	comm, err := edgetts.NewCommunicate(stripEmoji(text), config)
 	if err != nil {
 		return nil, err
 	}
